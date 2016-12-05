ee76f2e7f4ca17db609049a99e5539b71c2bd7af
rouz majlessi <rmajlessi@gmail.com>
2016-12-05 13:37:53 -0800

    can return a view

--- src/AjaxDemo/Controllers/HomeController.cs
+++ src/AjaxDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AjaxDemo.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,6 +23,15 @@ namespace AjaxDemo.Controllers
         public IActionResult Sum(int firstNumber, int secondNumber)
         {
             return Content((firstNumber + secondNumber).ToString(), "text/plain");
+        }
+        public IActionResult DisplayObject()
+        {
+            Destination destination = new Destination("Tokyo", "Japan", 1);
+            return Json(destination);
+        }
+        public IActionResult DisplayViewWithAjax()
+        {
+            return View();
         }
     }
 }
---
+++ src/AjaxDemo/Models/Destination.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AjaxDemo.Models
+{
+    public class Destination
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Id { get; set; }
+        public Destination(string city, string country, int id = 0)
+        {
+            City = city;
+            Country = country;
+            Id = id;
+        }
+    }
+}
